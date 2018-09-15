5ca11807cc331ae4a0772a7293ab7fcc57abdeb2
Benjamin Ameur <benjamin.ameur@flexybeauty.com>
2018-09-15 14:07:36 +0200

    added second route

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
+const nodeRequest = require('request')
+const assistant = require('actions-on-google')
+
 const app = express()
 
 app.get('/', function(req, res) {
     res.send('Hello World !')
+})
+
+app.get('/api/scene/kodi', function(req, res) {
+    res.send('Changing scene to kodi')
 })
 
 app.listen(5000, function() {
