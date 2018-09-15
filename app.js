bfd85f24198df215b6ccad03e4d7fdbb8a9401d3
Benjamin Ameur <benjamin.ameur@flexybeauty.com>
2018-09-15 16:39:42 +0200

    restored https response

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const nodeRequest = require('request')
 const functions = require('firebase-functions');
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
-const nodeRequest = require('request')
 
 const flow = dialogflow()
 const app = express()
