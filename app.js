a46c3a49f66108f33551e6aa589052df30121f08
Benjamin Ameur <benjamin.ameur@flexybeauty.com>
2018-09-15 15:33:03 +0200

    trying to have https installed

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+'use strict';
+
 const express = require('express')
 const nodeRequest = require('request')
 const assistant = require('actions-on-google')
+const dialogflow = assistant.dialogflow
 
 const app = express()
 
@@ -11,35 +14,39 @@ signals["DTV"] = [9150, 4352, 682, 446, 706, 422, 678, 448, 680, 448, 678, 448, 
 signals["Down"] = [9134, 4350, 678, 448, 682, 446, 704, 422, 680, 446, 708, 420, 682, 446, 680, 446, 680, 448, 682, 1574, 682, 1574, 678, 1574, 682, 1574, 680, 1576, 682, 1572, 704, 1550, 680, 448, 680, 1574, 682, 444, 678, 1576, 680, 1574, 682, 444, 678, 448, 682, 446, 682, 446, 706, 422, 706, 1546, 680, 448, 708, 418, 680, 1574, 680, 1574, 706, 1548, 682, 1572, 708]
 signals["Enter"] = [9152, 4350, 680, 446, 682, 446, 676, 450, 680, 448, 678, 448, 708, 420, 682, 446, 682, 444, 676, 1578, 680, 1574, 684, 1570, 682, 1574, 682, 1572, 708, 1546, 680, 1574, 678, 450, 682, 444, 680, 446, 678, 450, 678, 448, 708, 1546, 678, 448, 682, 446, 706, 418, 682, 1574, 678, 1576, 678, 1576, 680, 1574, 680, 448, 678, 1574, 682, 1572, 680, 1574, 682]
 
-app.get('/', function(req, res) {
-    res.send('Hello World !')
-})
 
-app.get('/api/scene/kodi', function(req, res) {
-    var timings = ""
-    for (var i = 0; i < signals["Source"].length; i++) {
-        timings += signals["Source"][i]
-        if (i != (signals["Source"].length - 1)) {
-            timings += ", "
-        }
-    }
-    var postBody = {
-        url: "http://192.168.1.56/play",
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        form: {
-            'timings': timings
-        }
-    };
-    nodeRequest(postBody, function(error, response, body) {
-        console.log(error)
-        console.log(body)
-    });
-    res.send(timings)
-})
 
-app.listen(5000, function() {
+// app.get('/', function(req, res) {
+//     res.send('Hello World !')
+// })
+
+// app.get('/api/scene/kodi', function(req, res) {
+//     var timings = ""
+//     for (var i = 0; i < signals["Source"].length; i++) {
+//         timings += signals["Source"][i]
+//         if (i != (signals["Source"].length - 1)) {
+//             timings += ", "
+//         }
+//     }
+//     var postBody = {
+//         url: "http://192.168.1.56/play",
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/x-www-form-urlencoded'
+//         },
+//         form: {
+//             'timings': timings
+//         }
+//     };
+//     nodeRequest(postBody, function(error, response, body) {
+//         console.log(error)
+//         console.log(body)
+//     });
+//     res.send(timings)
+// })
+
+app.use(express.static(__dirname, { dotfiles: 'allow' } ))
+
+app.listen(80, function() {
     console.log('Example app listening on port 5000')
 })
